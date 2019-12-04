ff7261ba479e27a5811f0d3e6eaa226ed16c2078
Alejandra Rodriguez <maarodriguezti@unal.edu.co>
2019-12-04 14:24:30 +0100

    Please again

--- app/src/main/java/com/example/projekt/MainActivity.java
+++ app/src/main/java/com/example/projekt/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action please", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action pleaseee", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
